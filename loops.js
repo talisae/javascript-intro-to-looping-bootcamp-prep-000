57c0e5e3390910d04ef13f8e29696c610eec38d9
Talisa Erazo <talisaerazo@gmail.com>
2018-09-20 02:01:32 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -3,7 +3,7 @@ function forLoop(array){
     if (i === 1){
       array.push("I am 1 strange loop.")
     }else{
-      array.push("")
+      array.push("I am ${i} strange loops.")
     }
-  }
+  }return 
 }
