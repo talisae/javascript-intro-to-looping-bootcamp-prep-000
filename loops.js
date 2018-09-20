44c03b3122d5e82300b311586b2b89f9fc3d850d
Talisa Erazo <talisaerazo@gmail.com>
2018-09-20 02:44:06 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -25,13 +25,3 @@ do {
 } while (array.length > 0 && incrementVariable());
 return array;
 
-for ([initialization]; [condition]; [iteration]) {
-  [loopBody];
-}
-while ([condition]) {
-  [loopBody];
-} // when we don't know how many times a loop needs to run - that is, the condition is dependent on a dynamic function/return value.
-do {
-  [loopBody];
-} while ([condition]);
-// do-while loop is almost exactly the same as the while loop, except for the fact that the loop's body is executed at least once before the condition is tested. 
