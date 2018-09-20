093c47d8222933093f1338d978d242d0e6eca202
Talisa Erazo <talisaerazo@gmail.com>
2018-09-20 02:48:28 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -15,6 +15,13 @@ function whileLoop(n){
   }
   return 'done';
 }
-function doWhileLoop(array){
-  
+function doWhileLoop(array) {
+  function maybeTrue() {
+    return Math.random() >= 0.5
+  }
+   do {
+    array.pop();
+  } while (array.length > -1 && maybeTrue());
+    return array;
 }
+
